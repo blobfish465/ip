d7b192203e6b68cc3c767cfd0fbf18e488d266ab
blobfish465 <ongqian2002@gmail.com>
2025-01-16 15:48:03 +0800

    Rename Duke to Blob, Add greeting and farewell

---
+++ src/main/java/Blob.java
@@ -0,0 +1,11 @@
+public class Blob {
+    public static void main(String[] args) {
+        String greeting = "Hello! I'm Blob\n"
+                + "What can I do for you?\n";
+
+        String farewell = "Bye. Hope to see you again soon!";
+
+        System.out.println(greeting);
+        System.out.println(farewell);
+    }
+}
