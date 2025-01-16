a9a2649ea99043708e50715a7fb0ca23afb34608
qi an <ongqian2002@gmail.com>
2025-01-16 16:38:03 +0800

    Add echo and bye command

--- src/main/java/Blob.java
+++ src/main/java/Blob.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Blob {
     public static void main(String[] args) {
         String greeting = "Hello! I'm Blob\n"
@@ -5,7 +7,26 @@ public class Blob {
 
         String farewell = "Bye. Hope to see you again soon!";
 
+        // Print greeting message
         System.out.println(greeting);
-        System.out.println(farewell);
+
+        // Create a new Scanner for user input
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                // Print farewell message
+                System.out.println(farewell);
+                break;
+            }
+
+            // Echo the user input
+            System.out.println(input);
+        }
+
+        // Close the scanner
+        scanner.close();
     }
 }
